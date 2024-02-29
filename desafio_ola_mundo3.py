c8b2b64c24a2240378d3897324e1acd0b286635c
Lucas Rocha <lukasrocha0402@gmail.com>
2024-02-29 09:08:42 -0300

    Adicionando o terceiro desafio e ultimo do modulo

---
+++ desafio_ola_mundo3.py
@@ -0,0 +1,10 @@
+numeroum = input ('Digite aqui o valor do primeiro numero=')
+numerodois = input('Digite aqui o valor do segundo numero=')
+
+valorum=int(numeroum)
+valordois=int(numerodois)
+
+
+soma = valorum + valordois
+
+print('A soma vale{}'.format(soma))
