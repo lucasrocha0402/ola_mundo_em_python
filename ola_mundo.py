671af7691b1809778d40e11e791e584e4766970d
Lucas Rocha <lukasrocha0402@gmail.com>
2024-02-29 09:05:07 -0300

    O primeiro olá mundo de python

---
+++ ola_mundo.py
@@ -0,0 +1,15 @@
+print('Primeiros passos')
+
+print(7+4)
+
+print('7' + '4')
+
+print('ola' , 5)
+
+nome = input('Qual é o seu nome ?')
+
+idade = input('Quantos anos você tem ?')
+
+peso = input('Quantos KG você pesa ?')
+
+print(nome,idade,peso)
